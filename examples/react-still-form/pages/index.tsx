5e9516198c7b8602e1b022a93a634b09e94fd347
honey32 <honey32@bearwapps.com>
2022-10-04 17:14:57 +0900

    feat(example): implemented example

--- examples/react-still-form/pages/index.tsx
+++ examples/react-still-form/pages/index.tsx
@@ -1,8 +1,41 @@
-import { NextPage } from "next"
+import { NextPage } from "next";
+import { createFormContext } from "@react-still-form/core";
+import { useCallback } from "react";
+
+const formContext = createFormContext();
+
+const JsonDisplay = () => {
+  const jsonStr = formContext.useSelector((s) =>
+    JSON.stringify(s.fields, null, 2)
+  );
+  return (
+    <div>
+      <pre>
+        <code>{jsonStr}</code>
+      </pre>
+    </div>
+  );
+};
+
+const Updater = () => {
+  const form = formContext.useContext();
+  const handleClick = useCallback(() => {
+    form.store.mutate((prev) => ({
+      ...prev,
+      fields: { a: { value: "aaaa", initialValue: "aaaa" } },
+    }));
+  }, [form.store]);
+  return <button onClick={handleClick}>クリック！</button>;
+};
 
 const IndexPage: NextPage = () => {
-  return <div>
-    
-  </div>
-}
-export default IndexPage
+  return (
+    <div>
+      <formContext.Provider>
+        <JsonDisplay />
+        <Updater />
+      </formContext.Provider>
+    </div>
+  );
+};
+export default IndexPage;
