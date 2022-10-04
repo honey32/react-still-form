2ad8f43c83eb09117b96bf18f57cfb9d1aef5a33
honey32 <honey32@bearwapps.com>
2022-10-04 11:34:14 +0900

    test: irrelevant field update to be ignored

--- packages/react-still-form/src/store/FormStore.test.ts
+++ packages/react-still-form/src/store/FormStore.test.ts
@@ -18,13 +18,19 @@ describe("FormStore", () => {
   });
 
   it("mutateField", () => {
-    const store = new FormStore(composeInitialFormState({ aaa: "a" }));
+    const store = new FormStore(
+      composeInitialFormState({ aaa: "a", bbb: "b" })
+    );
 
     const fn = vi.fn(() => {});
 
     const unsubscribe = store.subscribe(fn, { fields: { aaa: true } });
     store.mutateField("aaa", (prev) => prev);
     expect(fn).toBeCalled();
+
+    // irrelevant field update to be ignored
+    store.mutateField("bbb", (prev) => prev);
+    expect(fn).toBeCalledTimes(1);
 
     unsubscribe();
     store.mutateField("aaa", (prev) => prev);
